985f9a910ede717e7d2513992764757c7a60c7a1
Teodor Ande Elstad <teodor@andeelstad.com>
2019-04-07 17:22:07 +0200

    Simple EF Core database for SpeedTestResults

---
+++ SpeedTestApi/Database/SpeedTestDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SpeedTestApi.Database.Models;
+
+namespace SpeedTestApi.Database
+{
+    public class SpeedTestDbContext : DbContext
+    {
+        public SpeedTestDbContext(DbContextOptions<SpeedTestDbContext> options) : base(options) { }
+
+        public DbSet<SpeedTestResult> SpeedTestResults { get; set; }
+    }
+}
