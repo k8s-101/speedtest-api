0ff66cff52622c6add11e830f089af828da65516
Simen Granlund <simen@hgranlund.com>
2019-05-04 00:39:09 +0200

    Added basepath ion config

--- SpeedTestApi/Startup.cs
+++ SpeedTestApi/Startup.cs
@@ -10,60 +10,62 @@ using Swashbuckle.AspNetCore.Swagger;
 
 namespace SpeedTestApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<SpeedTestDbContext>(options => options.UseInMemoryDatabase("speedtest-api-database"));
-            services.AddTransient<ISpeedTestDbService, SpeedTestDbService>();
-
-            services
-                .AddCors()
-                .AddSwaggerGen(options =>
-                {
-                    options.SwaggerDoc("v1", new Info { Title = "SpeedTestApi", Version = "v1" });
-                })
-                .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-
-        public void Configure(IApplicationBuilder application, IHostingEnvironment environment)
-        {
-            if (environment.IsDevelopment())
-            {
-                application
-                    .UseDeveloperExceptionPage()
-                    .UseDummyDataSeed();
-            }
-            else
-            {
-                application
-                    .UseHsts()
-                    .UseHttpsRedirection();
-            }
-
-            application.UseCors(policy =>
-            {
-                policy.AllowAnyOrigin();
-                policy.AllowAnyMethod();
-                policy.AllowAnyHeader();
-                policy.AllowCredentials();
-            });
-
-            application
-                .UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "speedtest-api");
-                })
-                .UseSwagger()
-                .UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<SpeedTestDbContext>(options => options.UseInMemoryDatabase("speedtest-api-database"));
+      services.AddTransient<ISpeedTestDbService, SpeedTestDbService>();
+
+      services
+          .AddCors()
+          .AddSwaggerGen(options =>
+          {
+            options.SwaggerDoc("v1", new Info { Title = "SpeedTestApi", Version = "v1" });
+          })
+          .AddMvc()
+          .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+
+    public void Configure(IApplicationBuilder application, IHostingEnvironment environment)
+    {
+      if (environment.IsDevelopment())
+      {
+        application
+            .UseDeveloperExceptionPage()
+            .UseDummyDataSeed();
+      }
+      else
+      {
+        application
+            .UseHsts()
+            .UseHttpsRedirection();
+      }
+
+      application.UseCors(policy =>
+      {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+        policy.AllowCredentials();
+      });
+
+      application
+          .UseSwaggerUI(options =>
+          {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "speedtest-api");
+          })
+          .UseSwagger (opt => {
+                opt.PreSerializeFilters.Add ((swaggerDoc, httpReq) => swaggerDoc.BasePath = Configuration["hostBasePath"]);
+          })
+          .UseMvc();
+    }
+  }
 }
